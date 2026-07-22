a2d1363f4826d7f2ce52fb15a3afd6893d1b8f52
kabbott33 <kabbottvt@gmail.com>
2026-07-22 19:52:34 -0400

    Switched Wind Game to Arrow key controls

--- venera-lander/Assets/Scripts/WindManager.cs
+++ venera-lander/Assets/Scripts/WindManager.cs
@@ -33,11 +33,11 @@ public class WindManager : MonoBehaviour
     {
         float axisAdjustment = 0f;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             axisAdjustment += thrusterStrength;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             axisAdjustment -= thrusterStrength;
         }
